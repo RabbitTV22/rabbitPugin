d5f6f2ab7cc87751553b311bb3c87d115bd1e339
RabbitTV22 <154371299+RabbitTV22@users.noreply.github.com>
2025-04-14 20:21:31 -0400

    Upgrade to Paper

--- src/main/java/me/rabbittv/rabbit/EventHandlers.java
+++ src/main/java/me/rabbittv/rabbit/EventHandlers.java
@@ -1,6 +1,10 @@
 package me.rabbittv.rabbit;
 
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.minimessage.MiniMessage;
 import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -13,31 +17,29 @@ import org.bukkit.potion.PotionEffectType;
 import static org.bukkit.Bukkit.getServer;
 
 public class EventHandlers implements Listener {
+    private final Plugin Rabbit;
+    private final ConfigurationSection MessagesConfig;
+    private final ConfigurationSection Config;
+    private final MiniMessage miniMessage = MiniMessage.miniMessage();
 
-
-    @EventHandler
-    public void onPlayerJoin(PlayerJoinEvent event) {
-        Player player = event.getPlayer();
-        if (!player.hasPlayedBefore()) {
-            Bukkit.dispatchCommand(player, "spawn");
-        }
+    public EventHandlers(Plugin plugin, ConfigurationSection messagesConfig, ConfigurationSection Config) {
+        this.Rabbit = plugin;
+        this.MessagesConfig = messagesConfig;
+        this.Config = Config;
     }
+
 
     @EventHandler
     public void onPlayerDeath(PlayerDeathEvent event) {
         Player player = event.getEntity();
 
-        // Check if the player has the NIGHT_VISION effect
         if (player.hasPotionEffect(PotionEffectType.NIGHT_VISION)) {
-            // Store the duration and amplifier of the NIGHT_VISION effect
             PotionEffect nightVisionEffect = player.getPotionEffect(PotionEffectType.NIGHT_VISION);
             int duration = nightVisionEffect.getDuration();
             int amplifier = nightVisionEffect.getAmplifier();
 
-            // Respawn the player immediately
             player.spigot().respawn();
 
-            // Add a delay before applying the NIGHT_VISION effect to ensure it persists through respawn
             getServer().getScheduler().runTaskLater((Plugin) this, () -> {
                 player.addPotionEffect(new PotionEffect(PotionEffectType.NIGHT_VISION, duration, amplifier, false, false));
 
@@ -45,4 +47,21 @@ public class EventHandlers implements Listener {
         }
         Bukkit.dispatchCommand(player, "spawn");
     }
+
+    @EventHandler
+    public void onPlayerJoin(PlayerJoinEvent e) {
+        Player player = e.getPlayer();
+
+        if (!player.hasPlayedBefore()) {
+            Component joinMsgFirst = miniMessage.deserialize(MessagesConfig.getString("first_join_message", "<red><bold>Welcome %player% to the server for the first time!").replace("%player%", player.getName()));
+            e.joinMessage(joinMsgFirst);
+
+            Location teleportLocation = new Location(player.getWorld(), Config.getDouble("x", 2.5), Config.getDouble("y", 67.5), Config.getDouble("z", -5.5));
+            player.teleport(teleportLocation);
+        } else {
+            Component joinMsg = miniMessage.deserialize(MessagesConfig.getString("join_message", "<gray>%player% joined the game.").replace("%player%", player.getName()));
+            e.joinMessage(joinMsg);
+        }
+    }
+
 }
--- src/main/java/me/rabbittv/rabbit/Rabbit.java
+++ src/main/java/me/rabbittv/rabbit/Rabbit.java
@@ -3,9 +3,6 @@ package me.rabbittv.rabbit;
 import net.kyori.adventure.audience.Audience;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.minimessage.MiniMessage;
-import net.kyori.adventure.text.minimessage.tag.resolver.TagResolver;
-import net.kyori.adventure.text.minimessage.tag.standard.StandardTags;
-import org.bukkit.ChatColor;
 import org.bukkit.Location;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
@@ -21,7 +18,8 @@ import org.bukkit.potion.PotionEffectType;
 public final class Rabbit extends JavaPlugin implements Listener {
 
     private ConfigurationSection Config;
-    private ConfigurationSection MessagesConfig;
+    public ConfigurationSection MessagesConfig;
+    private final MiniMessage miniMessage = MiniMessage.miniMessage();
 
     @Override
     public void onEnable() {
@@ -29,14 +27,14 @@ public final class Rabbit extends JavaPlugin implements Listener {
         FileConfiguration config = getConfig();
         Config = config.getConfigurationSection("spawn_coords");
         MessagesConfig = config.getConfigurationSection("messages");
-        if (Config == null || Config == null) {
+        if (Config == null || MessagesConfig == null) {
             getLogger().warning("Configuration sections not found! Disabling plugin.");
             getServer().getPluginManager().disablePlugin(this);
             return;
         }
         getLogger().info("Sigma!");
         getServer().getPluginManager().registerEvents(this, this);
-        getServer().getPluginManager().registerEvents(new EventHandlers(), this);
+        getServer().getPluginManager().registerEvents(new EventHandlers(this, MessagesConfig, Config), this);
     }
 
     @Override
@@ -47,52 +45,46 @@ public final class Rabbit extends JavaPlugin implements Listener {
                 Location teleportLocation = new Location(player.getWorld(), Config.getDouble("x", 2.5), Config.getDouble("y", 67.5), Config.getDouble("z", -5.5));
                 player.teleport(teleportLocation);
                 Audience p = (Audience) sender;
-                var Spawn = MiniMessage.miniMessage();
-                Component Parsed = Spawn.deserialize(MessagesConfig.getString("teleport_to_spawn", "<gold>Teleporting to spawn..."));
+                Component Parsed = miniMessage.deserialize(MessagesConfig.getString("teleport_to_spawn", "<gold>Teleporting to spawn..."));
                 p.sendMessage(Parsed);
                 return false;
             } else {
                 Audience p = (Audience) sender;
-                var message = MiniMessage.miniMessage();
-                Component Parsed = message.deserialize(MessagesConfig.getString("not_a_player", "<dark_red>You have to be a player to use that command."));
+                Component Parsed = miniMessage.deserialize(MessagesConfig.getString("not_a_player", "<dark_red>You have to be a player to use that command."));
                 p.sendMessage(Parsed);
             }
         } else if (command.getName().equalsIgnoreCase("website")) {
             if (sender instanceof Player) {
                 Audience p = (Audience) sender;
-                var message = MiniMessage.miniMessage();
-                Component Parsed = message.deserialize(MessagesConfig.getString("website", "<red><b>Visit the website<br><click:open_url:https://www.rabbit-network.net>www.rabbit-network.net"));
+                Component Parsed = miniMessage.deserialize(MessagesConfig.getString("website", "<red><b>Visit the website<br><click:open_url:https://www.rabbit-network.net>www.rabbit-network.net"));
                 p.sendMessage(Parsed);
             } else if (sender instanceof ConsoleCommandSender) {
                 Audience p = (Audience) sender;
-                var message = MiniMessage.miniMessage();
-                Component Parsed = message.deserialize(MessagesConfig.getString("not_a_player", "<dark_red>You have to be a player to use that command."));
+                Component Parsed = miniMessage.deserialize(MessagesConfig.getString("not_a_player", "<dark_red>You have to be a player to use that command."));
                 p.sendMessage(Parsed);
             }
-            return true;
+            return false;
         } else if (command.getName().equalsIgnoreCase("nv")) {
             if (sender instanceof Player) {
                 Player player = (Player) sender;
                 Audience p = (Audience) sender;
-                var message = MiniMessage.miniMessage();
 
                 // Check if the player already has night vision
                 if (player.hasPotionEffect(PotionEffectType.NIGHT_VISION)) {
                     // If they have night vision, remove it
                     player.removePotionEffect(PotionEffectType.NIGHT_VISION);
-                    Component Parsed = message.deserialize(MessagesConfig.getString("nv_remove", "<blue>You removed you night vision effect."));
+                    Component Parsed = miniMessage.deserialize(MessagesConfig.getString("nv_remove", "<blue>You removed you night vision effect."));
                     p.sendMessage(Parsed);
                 } else {
                     // If they don't have night vision, give it to them
                     player.addPotionEffect(new PotionEffect(PotionEffectType.NIGHT_VISION, Integer.MAX_VALUE, 0, false, false));
-                    Component Parsed = message.deserialize(MessagesConfig.getString("nv_add", "<blue>You now have night vision."));
+                    Component Parsed = miniMessage.deserialize(MessagesConfig.getString("nv_add", "<blue>You now have night vision."));
                     p.sendMessage(Parsed);
                 }
                 return false;
             } else {
                 Audience p = (Audience) sender;
-                var message = MiniMessage.miniMessage();
-                Component Parsed = message.deserialize(MessagesConfig.getString("not_a_player", "<dark_red>You have to be a player to use that command."));
+                Component Parsed = miniMessage.deserialize(MessagesConfig.getString("not_a_player", "<dark_red>You have to be a player to use that command."));
                 p.sendMessage(Parsed);
             }
         }
